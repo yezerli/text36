e606e809930f4a208b80870444c9a14fafdf5cac
yezerli <1123603008@qq.com>
2020-03-06 14:40:12 +0800

    添加练习1文件

---
+++ text36/text1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <opencv.hpp>
+#include <stdio.h>
+
+using namespace cv;
+
+int main()
+{
+	cv::Mat src_color = imread("E:\\Git\\solar_cell_01.png");
+	std::vector<cv::Mat> channals;
+	cv::split(src_color, channals);
+	cv::Mat B = channals.at(0);
+	cv::Mat G = channals.at(1);
+	cv::Mat R = channals.at(2);
+	cv::imshow("red", R);
+	cv::imshow("blue", B);
+	cv::imshow("green", G);
+	cv::imshow("original Mat", src_color);
+	waitKey(0);
+}
